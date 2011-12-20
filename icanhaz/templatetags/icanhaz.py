9b39f85173857f2ca0ff57460e99ee6bc5730ba8
Reiner Marquez <rmaceissoft@gmail.com>
2011-12-20 12:41:12 -0500

    added verbatim template tag. allowed to use django template tags inside js template.

--- icanhaz/templatetags/icanhaz.py
+++ icanhaz/templatetags/icanhaz.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template import Template
 
 from ..conf import conf
 from ..loading import find, ICanHazTemplateNotFound
@@ -21,6 +22,8 @@ class ICanHazNode(template.Node):
             filepath = find(name)
             fp = open(filepath, "r")
             output = fp.read()
+            t = Template(output)
+            output = t.render(context)
             fp.close()
             output = ('<script id="%s" type="text/html">\n'
                       % name) + output + "\n</script>\n"
@@ -45,3 +48,69 @@ def icanhaz(parser, token):
         raise template.TemplateSyntaxError(
             "'icanhaz' tag takes one argument: the name/id of the template")
     return ICanHazNode(bits[1])
+
+
+class VerbatimNode(template.Node):
+    def __init__(self, text_and_nodes):
+        self.text_and_nodes = text_and_nodes
+
+    def render(self, context):
+        output = ""
+
+        # If its text we concatenate it, otherwise it's a node and we render it
+        for bit in self.text_and_nodes:
+            if isinstance(bit, basestring):
+                output += bit
+            else:
+                output += bit.render(context)
+
+        return output
+
+@register.tag
+def verbatim(parser, token):
+    """
+    Copied from https://gist.github.com/893408
+
+    Wrap {% verbatim %} and {% endverbatim %} around those
+    blocks of jQuery templates to avoid conflict with django variable templates {{ }}
+
+    this template tag allows you to use tags like url {% url name %}, {% trans "bar" %} or {% csrf_token %}
+    on mustache templates.
+
+    Thanks to Miguel Angel Araujo & Eric Florenzano for this code
+    """
+    text_and_nodes = []
+    while 1:
+        token = parser.tokens.pop(0)
+        if token.contents == 'endverbatim':
+            break
+
+        if token.token_type == template.TOKEN_VAR:
+            text_and_nodes.append('{{')
+            text_and_nodes.append(token.contents)
+
+        elif token.token_type == template.TOKEN_TEXT:
+            text_and_nodes.append(token.contents)
+
+        elif token.token_type == template.TOKEN_BLOCK:
+            try:
+                command = token.contents.split()[0]
+            except IndexError:
+                parser.empty_block_tag(token)
+
+            try:
+                compile_func = parser.tags[command]
+            except KeyError:
+                parser.invalid_block_tag(token, command, None)
+            try:
+                node = compile_func(parser, token)
+            except template.TemplateSyntaxError, e:
+                if not parser.compile_function_error(token, e):
+                    raise
+
+            text_and_nodes.append(node)
+
+        if token.token_type == template.TOKEN_VAR:
+            text_and_nodes.append('}}')
+
+    return VerbatimNode(text_and_nodes)
